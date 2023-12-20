7e9820bff4e469612d72f2c1f029ed08444be374
gladysuc31 <gladyslizeth31@gmail.com>
2023-12-19 23:29:58 -0500

    segundo commit

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,12 +1,24 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(/\s+/);
+    // Contar la cantidad de palabras utilizando length
+    const wordCount = words.length;
+    // Retornar el resultado
+    return wordCount;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text;
+    const characterCount = words.length;
+    return characterCount;
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(" ").join("");
+    const characterCount = words.length;
+    return characterCount;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+// boton de reset 
+const resetButton = document.getElementById("reset-button");
+const areabox = document.querySelector("textarea");
+
+resetButton.addEventListener("click",function() {
+    areabox.value = "";
+})
+
+//para las funciones de los elementos
+const wordCount = document.querySelector("[data-testid=word-count]")
+const characterCount = document.querySelector("[data-testid=character-count]")
+const characterNoSpacesCount = document.querySelector("[data-testid=character-no-spaces-count]")
+const numberCount = document.querySelector("[data-testid=number-count]")
+const numberSum = document.querySelector("[data-testid=number-sum]")
+const wordLength = document.querySelector("[data-testid=word-length-average]")
+
+
+// agregar un event listener
+areabox.addEventListener("input",function(){
+    //guardar el input ingresado
+    const inputText = areabox.value;
+    //llamar al analyzer con el valor del argumento guardado
+    const updatedText = analyzer.getWordCount(inputText)
+    wordCount.textContent = "Palabras " + updatedText
+})
+
+areabox.addEventListener("input",function(){
+    //guardar el input ingresado
+    const inputText = areabox.value;
+    //llamar al analyzer con el valor del argumento guardado
+    const updatedText = analyzer.getCharacterCount(inputText)
+    characterCount.textContent = "Caracteres " + updatedText
+})
+
+areabox.addEventListener("input",function(){
+    //guardar el input ingresado
+    const inputText = areabox.value;
+    //llamar al analyzer con el valor del argumento guardado
+    const updatedText = analyzer.getCharacterCountExcludingSpaces(inputText)
+    characterNoSpacesCount.textContent = "Caracteres sin espacio " + updatedText
+})
+
+areabox.addEventListener("input",function(){
+    //guardar el input ingresado
+    const inputText = areabox.value;
+    //llamar al analyzer con el valor del argumento guardado
+    const updatedText = analyzer.getNumberCount(inputText)
+    numberCount.textContent = "Numeros " + updatedText
+})
+
+areabox.addEventListener("input",function(){
+    //guardar el input ingresado
+    const inputText = areabox.value;
+    //llamar al analyzer con el valor del argumento guardado
+    const updatedText = analyzer.getNumberSum(inputText)
+    numberSum.textContent = "Suma numeros " + updatedText
+})
+
+areabox.addEventListener("input",function(){
+    //guardar el input ingresado
+    const inputText = areabox.value;
+    //llamar al analyzer con el valor del argumento guardado
+    const updatedText = analyzer.getAverageWordLength(inputText)
+    wordLength.textContent = "Promedio Longitud " + updatedText
+})
