a5017a18d2d3195e0e9b0569955b216cc5005b0a
gladysuc31 <gladyslizeth31@gmail.com>
2024-01-17 11:45:59 -0500

    Updates

---
+++ analyzer.js
@@ -0,0 +1,85 @@
+const analyzer = {  
+  getWordCount: (text) => {
+    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(/\s+/);
+    // Contar la cantidad de palabras utilizando length
+    const wordCount = words.length;
+    // Retornar el resultado
+    return wordCount;
+  },
+  getCharacterCount: (text) => {
+    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text;
+    const characterCount = words.length;
+    return characterCount;
+  },
+  getCharacterCountExcludingSpaces: (text) => {
+    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    // declarar la constante que tendra el texto que se ponga en el input y separarlo en un array
+    // crear el for..of loop que iterara sobre los caracteres
+    // si el caracter es una letra que lo tome en cuenta
+    // si no es, no tomarlo en cuenta
+    const words = text;
+    let count = 0;
+
+    for (const currentletter of words) {
+      if(currentletter.match(/[a-zA-Z]/)) {
+        count++;
+      }
+    }
+    return count;
+  },
+  getAverageWordLength: (text) => { 
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    // identificar cuantas palabras hay
+    // calcular la longitud de cada palabra
+    // calcular el promedio 
+    const words = text.split(/\s+/);
+
+    
+    let wordLength = 0;
+
+    for (const currentWord of words) {
+      wordLength += currentWord.length;
+    }
+
+    const average = wordLength / words.length;
+    return parseFloat(average.toFixed(2));
+  },
+  getNumberCount: (text) => {
+    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.split(" ");
+    let numberCounter = 0;
+    
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i].match(/^[0-9]+(\.[0-9]+)?$/)) {
+        numberCounter++;
+      }
+    }
+    
+    return numberCounter;
+  },
+  getNumberSum: (text) => {
+    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    // determinar cuales son los numeros
+    // separarlos en un array
+    // sumar los numeros
+    // retornar el resultado
+    const words = text.split(" ");
+    const numbersArray = [];
+    let totalSum = 0;
+
+    for (const word of words){
+      if (word.match(/^[0-9]+(\.[0-9]+)?$/)){
+        const number = parseFloat(word);
+        numbersArray.push(number);
+      }
+    }
+
+    for (const num of numbersArray){
+      totalSum += num;
+    } 
+    return totalSum;
+  },
+};
+export default analyzer;
---
+++ index.js
@@ -0,0 +1,50 @@
+import analyzer from './analyzer.js';
+
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+// boton de reset 
+const resetButton = document.getElementById("reset-button");
+const areabox = document.querySelector("textarea");
+
+resetButton.addEventListener("click",function() {
+  areabox.value = "";
+  wordCount.textContent = "Palabras "
+  characterCount.textContent = "Caracteres "
+  characterNoSpacesCount.textContent = "Caracteres sin espacio "
+  numberCount.textContent = "Numeros "
+  numberSum.textContent = "Suma numeros "
+  wordLength.textContent = "Promedio Longitud "
+})
+
+//para las funciones de los elementos
+const wordCount = document.querySelector("[data-testid=word-count]")
+const characterCount = document.querySelector("[data-testid=character-count]")
+const characterNoSpacesCount = document.querySelector("[data-testid=character-no-spaces-count]")
+const numberCount = document.querySelector("[data-testid=number-count]")
+const numberSum = document.querySelector("[data-testid=number-sum]")
+const wordLength = document.querySelector("[data-testid=word-length-average]")
+
+
+// agregar un event listener
+areabox.addEventListener("input",function(){
+  //guardar el input ingresado
+  const inputText = areabox.value;
+  //llamar al analyzer con el valor del argumento guardado
+  const wordCountText = analyzer.getWordCount(inputText)
+  wordCount.textContent = "Palabras " + wordCountText
+
+  const characterCountText = analyzer.getCharacterCount(inputText)
+  characterCount.textContent = "Caracteres " + characterCountText
+
+  const characterCountExcludingSpacesText = analyzer.getCharacterCountExcludingSpaces(inputText)
+  characterNoSpacesCount.textContent = "Caracteres sin espacio " + characterCountExcludingSpacesText
+
+  const numberCountText = analyzer.getNumberCount(inputText)
+  numberCount.textContent = "Numeros " + numberCountText
+
+  const numberSumText = analyzer.getNumberSum(inputText)
+  numberSum.textContent = "Suma numeros " + numberSumText
+
+  const averageWordLengthText = analyzer.getAverageWordLength(inputText)
+  wordLength.textContent = "Promedio Longitud " + averageWordLengthText
+})
